588a63ac3766ca12a7fbdea27fdb8c12109654c7
Khushboo <khushbooatwork30@gmail.com>
2024-12-10 16:51:44 +0530

    make a new file practice3Sketch.js in which circle move around mouse

--- cha-1-vector/practice2Sketch.js
+++ cha-1-vector/practice2Sketch.js
@@ -1,6 +1,31 @@
+let walker;
 function setup(){
-    createCanvas(400,400)
+    createCanvas(1500,900);
+    background(0);
+    walker = new Walker(width/2, height/2);
+
 }
 function draw(){
-    background(0)
+    walker.update();
+    walker.show();
+
 }
+
+class Walker{
+    constructor(x,y){
+        this.pos = createVector(x,y);
+        // this.vel = createrVector(1,-1)
+        this.vel = p5.Vector.random2D();
+        this.vel.mult(random(3))
+    }
+    update(){
+        this.pos.add(this.vel);
+    }
+    show(){
+        stroke("green");
+        strokeWeight(2);
+        fill("green")
+        ellipse(this.pos.x, this.pos.y, 32);
+     }
+}
+ 
