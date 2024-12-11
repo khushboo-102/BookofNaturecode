f76017533821805bb7c5be9ddcf4d8216ca43a50
Khushboo <khushbooatwork30@gmail.com>
2024-12-11 10:55:49 +0530

    add new file practice4Sketch.js

--- cha-1-vector/practice2Sketch.js
+++ cha-1-vector/practice2Sketch.js
@@ -1,31 +1,30 @@
 let walker;
-function setup(){
-    createCanvas(1500,900);
+function setup() {
+    createCanvas(1500, 900);
     background(0);
-    walker = new Walker(width/2, height/2);
+    walker = new Walker(width / 2, height / 2);
 
 }
-function draw(){
+function draw() {
     walker.update();
     walker.show();
 
 }
 
-class Walker{
-    constructor(x,y){
-        this.pos = createVector(x,y);
+class Walker {
+    constructor(x, y) {
+        this.pos = createVector(x, y);
         // this.vel = createrVector(1,-1)
         this.vel = p5.Vector.random2D();
         this.vel.mult(random(3))
     }
-    update(){
+    update() {
         this.pos.add(this.vel);
     }
-    show(){
+    show() {
         stroke("green");
         strokeWeight(2);
         fill("green")
         ellipse(this.pos.x, this.pos.y, 32);
-     }
+    }
 }
- 
