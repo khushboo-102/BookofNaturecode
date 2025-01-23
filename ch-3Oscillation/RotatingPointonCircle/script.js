d3295ef56f7bd56947001afc563728f5b318748a
Khushboo <khushbooatwork30@gmail.com>
2025-01-23 11:10:48 +0530

    tried new experiment

--- ch-3Oscillation/RotatingPointonCircle/script.js
+++ ch-3Oscillation/RotatingPointonCircle/script.js
@@ -19,22 +19,23 @@
 //        angle += 0.07; // Increment the angle rotate speed 
 
 // }
- 
+
 
 //============Spiraling Point Animation==========================
-let angle = 0;
-let r= 150;
-function setup(){
-    createCanvas(400,400);
-    background(154,0,0);
+let angle = 0; // Initialize the angle variable to control the point's rotation
+let r = 150;  // Set the initial radius of the spiral
+function setup() {
+    createCanvas(400, 400); // Create a canvas of size 400x400
+    background(154, 0, 0); // Set the background color
 }
-function draw(){
-    translate(200,200);
-    strokeWeight(10);
-    stroke(255,238,33)
+function draw() {
+    translate(200, 200); // Move the origin to the center of the canvas
+    strokeWeight(10); // Set the thickness of the point
+    stroke(255, 238, 33) // Set the color of the point to yellow
+    // Calculate the x and y positions of the point using trigonometry
     let x = r * cos(angle)
     let y = r * sin(angle)
-    point(x,y)
-    angle += 0.04
+    point(x, y) // Draw the point at the calculated position
+    angle += 0.04 // Increment the angle to create the rotation effect=
     r -= 0.2
 }
